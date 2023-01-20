d5aa01851ba9bc97873c394d92e58508eada11fa
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-20 13:08:09 -0500

    screenCap now saving

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const db = require("./db");
+const ScreenCaps = require("./models/ScreenCaps");
+const Tags = require("./models/tags");
+
+Tags.belongsToMany(ScreenCaps, { through: "media_tags" });
+ScreenCaps.belongsToMany(Tags, { through: "media_tags" });
+
+db.sync();
+module.exports = {
+	db,
+	Tags,
+	ScreenCaps,
+};
---
+++ db/models/Tags.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Tags = db.define("tag", {
+    tagName:{
+        type: Sequelize.STRING,
+        allowNull:false,
+    }
+})
+
+module.exports = Tags
+
