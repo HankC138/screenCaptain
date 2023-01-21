6f2f15565727bc882bb1e3ac0b8e6cb47f2eb604
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-20 23:40:51 -0500

    associations working, search backend working

--- db/models/Capture.js
+++ db/models/Capture.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize')
-const db = require('../db')
+const db = require('../db');
 
-const Media = db.define("media", {
+const Capture = db.define("capture", {
     location:{
         type: Sequelize.STRING,
         allowNull:false,
     }
 })
 
-module.exports = Media
+module.exports = Capture
