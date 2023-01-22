308cfcfdb06b75e1f718dc6bb8e90b387a828453
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-21 19:02:14 -0500

    pulling images from the cloud now

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ export default function App() {
   const [mediaId, setMediaId] = useState()
   const [tags, setTags] = useState([])
   const [currentTag, setCurrentTag] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [queryReturn, setQueryReturn] = useState([])
 
   useEffect(() => {
-    window.mediaCapListener.mediaCaptureEvent((event, value)=>{
+    window.mediaCapListener.mediaCaptureEvent((_event, value)=>{
       setMediaData(value)
-    window.updatedTags.updateTagsEvent((event, value) =>{
+    window.updatedTags.updateTagsEvent((_event, value) =>{
       setMediaId(value)
     })
     })
@@ -25,26 +27,43 @@ export default function App() {
     setCurrentTag([])
   }
 
-  const handleInputChange = (event) =>{
+  const handleTagInputChange = (event) =>{
     setCurrentTag(event.target.value)
   }
 
+  const handleSearchInputChange = (event) =>{
+    setSearchTerm(event.target.value)
+  }
+  const handleSearch = async () =>{
+    const searchData = await window.search.mediaTagSearch(searchTerm)
+    setQueryReturn(JSON.parse(searchData))
+  }
+
   const handleRemoveTag = (event) =>{
-    setTags(tags.filter((tag,index)=> index !== event))
+    setTags(tags.filter((_tag,index)=> index !== event))
+  }
+  
+  const handleRemoveMedia = (_event) =>{
+    setMediaData()
   }
 
   const handleTagSave = () =>{
     window.tagsSave.sendTags({mediaId, tags})
-  }
-
+    setTags([])
+    setMediaData()
+    }
+  
   return (
     <>
-      <button onClick={handleCaptureClick}>Capture</button><button onClick={handleTagSave}>Save Tags to Screen Cap</button><br/>
+      <button disabled={mediaData} onClick={handleCaptureClick}>Capture</button><button onClick={handleTagSave}>Attach Tags to Media</button><br/>
       <form onSubmit={onSubmitTag}>
-      <input type="text" placeholder="tags" value={currentTag} onChange={handleInputChange}></input><button type="submit">Add Tag</button><br/>
+      <input type="text" placeholder="tag to add" value={currentTag} onChange={handleTagInputChange}></input><button type="submit" disabled={!currentTag.length}>Add Tag</button><br/>
       </form><br/>
       <ul>{tags ? tags.map((tag, index)=> <li key={`tag ${index}`}><button onClick={() =>handleRemoveTag(index)}>X</button>{" "}{tag}</li>):null}</ul>
-      <img src={mediaData}></img>
+      <input type="text" placeholder="tag search" value={searchTerm} onChange={handleSearchInputChange}></input><button onClick={handleSearch} disabled={!searchTerm}>Search</button><br/>
+      <img onClick={handleRemoveMedia} src={mediaData}></img>
+    {queryReturn ? queryReturn.map((result)=> <img key={result.location} src={result.location}/>) : null}
     </>
   );
 }
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 const { app, BrowserWindow, desktopCapturer, ipcMain } = require("electron");
 const { writeFile } = require("fs");
 const crypto = require("crypto");
-const { saveMediaLocation, saveTags } = require("../db");
+const { saveMediaLocation, saveTags, searchMediaTags } = require("../db");
+
+import { S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+
+const REGION = "us-east-1";
+const s3Client = new S3Client({ region: REGION });
 
 if (require("electron-squirrel-startup")) {
 	app.quit();
@@ -45,12 +51,27 @@ ipcMain.on("media-capture", (_event, _value) => {
 		.then((sources) => {
 			const screenPng = sources[0].thumbnail.toPNG();
 			const image = sources[0].thumbnail.toDataURL();
-			const location = `${process.cwd()}/mediaCaptures/${crypto.randomUUID()}.png`;
-			writeFile(location, screenPng, (err) => {
-				if (err) throw err;
-				console.log("The file has been saved!");
-			});
-
+			const newUUID = crypto.randomUUID();
+			const fileName = `${newUUID}.png`;
+			const location = `https://mytestbucketformediacaptain.s3.us-east-1.amazonaws.com/${fileName}`
+			const bucketParams = {
+				Bucket: "mytestbucketformediacaptain",
+				Key: fileName,
+				Body: screenPng,
+			};
+			(async () => {
+				try {
+					await s3Client.send(new PutObjectCommand(bucketParams));
+					return console.log(
+						"Successfully uploaded object: " +
+							bucketParams.Bucket +
+							"/" +
+							bucketParams.Key
+					);
+				} catch (err) {
+					console.log("Error", err);
+				}
+			})();
 			saveMediaLocation(location)
 				.then((mediaCapture) => {
 					mainWindow.webContents.send(
@@ -67,4 +88,8 @@ ipcMain.on("media-capture", (_event, _value) => {
 
 ipcMain.on("tag-save", (e, value) => {
 	saveTags(value);
+});
+
+ipcMain.handle("media-tag-search", (e, value) => {
+	return searchMediaTags(value);
 });
